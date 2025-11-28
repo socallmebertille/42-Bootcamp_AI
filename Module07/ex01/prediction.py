9b6338656b1850b25c405dab0e8b619c8e9c9637
socallmebertille <sarah.bejard@gmail.com>
2025-11-28 08:54:44 +0100

    Module07/ex01 done

--- Module07/ex01/prediction.py
+++ Module07/ex01/prediction.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def simple_predict(x, theta):
+def predict_(x, theta):
     """Computes the prediction vector y_hat from two non-empty numpy.array.
     Args:
-        x: has to be an numpy.array, a matrix of dimension m * n.
-        theta: has to be an numpy.array, a vector of dimension (n + 1) * 1.
+        x: has to be an numpy.array, a vector of dimensions m * n.
+        theta: has to be an numpy.array, a vector of dimensions (n + 1) * 1.
     Return:
-        y_hat as a numpy.array, a vector of dimension m * 1.
+        y_hat as a numpy.array, a vector of dimensions m * 1.
         None if x or theta are empty numpy.array.
-        None if x or theta dimensions are not matching.
+        None if x or theta dimensions are not appropriate.
         None if x or theta is not of expected type.
     Raises:
         This function should not raise any Exception.
@@ -38,28 +38,28 @@ def main():
     theta1 = np.array([5, 0, 0, 0]).reshape((-1, 1))
     print("theta1 : \n", theta1)
 
-    print("predict : \n", simple_predict(x, theta1))
+    print("predict : \n", predict_(x, theta1))
     print("Expected : array([[5.], [5.], [5.], [5.]])")
 
     print("============= 2 ===================")
     theta2 = np.array([0, 1, 0, 0]).reshape((-1, 1))
     print("theta2 : \n", theta2)
 
-    print("predict : \n", simple_predict(x, theta2))
+    print("predict : \n", predict_(x, theta2))
     print("Expected : array([[ 1.], [ 4.], [ 7.], [10.]])")
 
     print("============= 3 ===================")
     theta3 = np.array([-1.5, 0.6, 2.3, 1.98]).reshape((-1, 1))
     print("theta3 : \n", theta3)
 
-    print("predict : \n", simple_predict(x, theta3))
+    print("predict : \n", predict_(x, theta3))
     print("Expected : array([[ 9.64], [24.28], [38.92], [53.56]])")
 
     print("============= 4 ===================")
     theta4 = np.array([-3, 1, 2, 3.5]).reshape((-1, 1))
     print("theta4 : \n", theta4)
 
-    print("predict : \n", simple_predict(x, theta4))
+    print("predict : \n", predict_(x, theta4))
     print("Expected : array([[12.5], [32. ], [51.5], [71. ]])")
 
     return 0
