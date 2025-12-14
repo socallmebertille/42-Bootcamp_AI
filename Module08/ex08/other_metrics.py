e73b603a97ab5ba5a358cd4964fb563745c96b04
socallmebertille <sarah.bejard@gmail.com>
2025-12-14 20:21:12 +0100

    Module08/ex08 done

--- Module08/ex08/other_metrics.py
+++ Module08/ex08/other_metrics.py
@@ -2,63 +2,69 @@ import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def accuracy_score_(y, y_hat):
-    """
-    Compute the accuracy score.
+    """Compute the accuracy score.
     Args:
-    y:a numpy.ndarray for the correct labels
-    y_hat:a numpy.ndarray for the predicted labels
+        y : a numpy.ndarray for the correct labels
+        y_hat : a numpy.ndarray for the predicted labels
     Returns:
-    The accuracy score as a float.
-    None on any error.
+        The accuracy score as a float.
+        None on any error.
     Raises:
-    This function should not raise any Exception.
+        This function should not raise any Exception.
     """
-    return
+    # objets classés correctement / tous les objets
+    accuracy = np.mean(y_hat == y)
+    return accuracy
 
 def precision_score_(y, y_hat, pos_label=1):
-    """
-    Compute the precision score.
+    """Compute the precision score.
     Args:
-    y:a numpy.ndarray for the correct labels
-    y_hat:a numpy.ndarray for the predicted labels
-    pos_label: str or int, the class on which to report the precision_score (default=1)
+        y : a numpy.ndarray for the correct labels
+        y_hat : a numpy.ndarray for the predicted labels
+        pos_label : str or int, the class on which to report the precision_score (default=1)
     Returns:
-    The precision score as a float.
-    None on any error.
+        The precision score as a float.
+        None on any error.
     Raises:
-    This function should not raise any Exception.
+        This function should not raise any Exception.
     """
-    return
+    # objets correctement classés pos_label / tout ceux classés pos_label
+    precision = np.sum((y_hat == pos_label) & (y == pos_label)) / np.sum(y_hat == pos_label)
+    return precision
 
 def recall_score_(y, y_hat, pos_label=1):
-    """
-    Compute the recall score.
+    """Compute the recall score.
     Args:
-    y:a numpy.ndarray for the correct labels
-    y_hat:a numpy.ndarray for the predicted labels
-    pos_label: str or int, the class on which to report the precision_score (default=1)
+        y : a numpy.ndarray for the correct labels
+        y_hat : a numpy.ndarray for the predicted labels
+        pos_label : str or int, the class on which to report the precision_score (default=1)
     Returns:
-    The recall score as a float.
-    None on any error.
+        The recall score as a float.
+        None on any error.
     Raises:
-    This function should not raise any Exception.
+        This function should not raise any Exception.
     """
-    return
+    # objets classés correctement pos_label / tous ceux réellement pos_label
+    recall = np.sum((y_hat == pos_label) & (y == pos_label)) / np.sum(y == pos_label)
+    return recall
 
 def f1_score_(y, y_hat, pos_label=1):
-    """
-    Compute the f1 score.
+    """Compute the f1 score.
     Args:
-    y:a numpy.ndarray for the correct labels
-    y_hat:a numpy.ndarray for the predicted labels
-    pos_label: str or int, the class on which to report the precision_score (default=1)
+        y : a numpy.ndarray for the correct labels
+        y_hat : a numpy.ndarray for the predicted labels
+        pos_label : str or int, the class on which to report the precision_score (default=1)
     Returns:
-    The f1 score as a float.
-    None on any error.
+        The f1 score as a float.
+        None on any error.
     Raises:
-    This function should not raise any Exception.
+        This function should not raise any Exception.
     """
-    return
+    # compromis entre précision et sensibilité
+    precision = precision_score_(y, y_hat, pos_label)
+    recall = recall_score_(y, y_hat, pos_label)
+    f1_score = 2 * precision * recall / (precision + recall)
+    return f1_score
 
 def main():
     """Tester of my functions of precision indicators"""
