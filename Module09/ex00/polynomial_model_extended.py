d872db530b6fefbce88f539e84a8246a2abddbb4
socallmebertille <sarah.bejard@gmail.com>
2025-12-14 23:10:24 +0100

    Module09/ex00 done

--- Module09/ex00/polynomial_model_extended.py
+++ Module09/ex00/polynomial_model_extended.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import combinations_with_replacement
 
 def add_polynomial_features(x, power):
     """Add polynomial features to matrix x by raising its columns to every power in the range
@@ -15,23 +16,26 @@ def add_polynomial_features(x, power):
     Raises:
         This function should not raise any Exception.
     """
-    if not isinstance(x, np.ndarray):
+    if not isinstance(x, np.ndarray) or x.ndim != 2:
         return None
-    if x.size == 0:
+    if not isinstance(power, int) or power < 1:
         return None
-    if x.ndim != 2 or x.shape[1] != 1:
-        return None
-    return 
+
+    m, n = x.shape
+    X_poly = np.ones((m, 0), dtype=int)
+    for p in range(1, power + 1):
+        X_poly = np.hstack((X_poly, x ** p))
+    return X_poly
 
 def main():
     """Tester of my function who add polynomial features to x"""
 
     print("============= TEST ===================")
-
-    print("============= 1 ===================")
     x = np.arange(1,11).reshape(5, 2)
     print("x array : \n", x)
-    print("sigmoid : \n", add_polynomial_features(x, 3))
+
+    print("============= 1 ===================")
+    print("add polynomial features to x : \n", add_polynomial_features(x, 3))
     print("Expected : array([[ 1, 2, 1, 4, 1, 8], \
                     \n\t\t[ 3, 4, 9, 16, 27, 64], \
                     \n\t\t[ 5, 6, 25, 36, 125, 216], \
@@ -39,9 +43,7 @@ def main():
                     \n\t\t[ 9, 10, 81, 100, 729, 1000]])")
 
     print("============= 2 ===================")
-    x = np.array([[2]])
-    print("x array : \n", x)
-    print("sigmoid : \n", add_polynomial_features(x, 4))
+    print("add polynomial features to x : \n", add_polynomial_features(x, 4))
     print("Expected : array([[ 1, 2, 1, 4, 1, 8, 1, 16], \
                         \n\t\t[ 3, 4, 9, 16, 27, 64, 81, 256], \
                         \n\t\t[ 5, 6, 25, 36, 125, 216, 625, 1296], \
