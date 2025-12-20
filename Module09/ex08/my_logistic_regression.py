1603d29b8cf5a10d0e1c9707453117891b9b64b9
socallmebertille <sarah.bejard@gmail.com>
2025-12-20 23:01:40 +0100

    Module09/ex08 done

--- Module09/ex08/my_logistic_regression.py
+++ Module09/ex08/my_logistic_regression.py
@@ -119,3 +119,53 @@ class MyLogisticRegression():
         std = x.std(axis=0)
         X_norm = (x - mean) / std
         return X_norm, mean, std
+
+
+def main():
+    """Test Logistic Ridge Regression"""
+
+    # valeurs expected encore erronnees mais avec lambda 0.0 on s'y rapproche le +
+
+    print("============= TEST ===================")
+
+    X = np.array([[1., 1., 2., 3.], [5., 8., 13., 21.], [3., 5., 9., 14.]])
+    Y = np.array([[1], [0], [1]])
+    thetas = np.array([[2], [0.5], [7.1], [-4.3], [2.09]])
+    
+    mylr = MyLogisticRegression(thetas, alpha=0.001, max_iter=10000, lambda_=0.0)
+
+    print("X array : \n", X)
+    print("Y array : \n", Y)
+    print("Initial theta : \n", thetas)
+
+    print("\n============= 1 - Initial Prediction ===================")
+    y_hat = mylr.predict_(X)
+    print("predict : \n", y_hat)
+    print("Expected : array([[0.99930437],\n\t\t[1. ],\n\t\t[1. ]])")
+
+    print("\n============= 2 - Initial Loss ===================")
+    loss = mylr.loss_(Y, y_hat)
+    print("loss : ", loss)
+    print("Expected : 11.513157421577002")
+
+    print("\n============= 3 - Training ===================")
+    print("Training with alpha={}, max_iter={}, lambda_={}...".format(
+    mylr.alpha, mylr.max_iter, mylr.lambda_))
+    mylr.fit_(X, Y)
+    print("thetas trained : \n", mylr.theta)
+    print("Expected : array([[ 2.11826435]\n\t\t[ 0.10154334]\n\t\t[ 6.43942899]\n\t\t[-5.10817488]\n\t\t[ 0.6212541 ]])")
+
+    print("\n============= 4 - Prediction after training ===================")
+    y_hat = mylr.predict_(X)
+    print("predict : \n", y_hat)
+    print("Expected : array([[0.57606717]\n\t\t[0.68599807]\n\t\t[0.06562156]])")
+
+    print("\n============= 5 - Final Loss ===================")
+    loss = mylr.loss_(Y, y_hat)
+    print("loss : ", loss)
+    print("Expected : 1.4779126923052268")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
