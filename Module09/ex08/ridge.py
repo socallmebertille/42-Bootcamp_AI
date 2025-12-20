8955b65adf669d7b0903224121e762276dd09553
socallmebertille <sarah.bejard@gmail.com>
2025-12-20 20:39:55 +0100

    Module09/ex06 done

--- Module09/ex08/ridge.py
+++ Module09/ex08/ridge.py
@@ -57,7 +57,7 @@ class MyRidge(MyLinearRegression):
         """
         Computes the regularized gradient
         """
-        return vec_reg_linear_grad(y, x, self.thetas, self.lambda_)
+        return vec_reg_linear_grad(x, y, self.thetas, self.lambda_)
     
     def fit_(self, x, y):
         if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray):
@@ -66,7 +66,7 @@ class MyRidge(MyLinearRegression):
             return None
         new_theta = np.copy(self.thetas)
         for _ in range(self.max_iter):
-            grad = vec_reg_linear_grad(y, x, new_theta, self.lambda_)
+            grad = vec_reg_linear_grad(x, y, new_theta, self.lambda_)
             if grad is None:
                 return None
             new_theta = new_theta - self.alpha * grad
@@ -77,9 +77,6 @@ def main():
     """Univariate Logistic Regression
     Train three different univariate models to predict spacecraft prices.
     """
-
-    # valeurs attendues dans le sujet erronnes => proviennent de resultat attendus en regression logistique
-    # expected updated :
 
     print("============= TEST ===================")
 
@@ -95,25 +92,25 @@ def main():
     print("============= 1 ===================")
     y_hat = mylr.predict_(X)
     print("predict : \n", y_hat)
-    print("Expected : array([[7.27],\n\t\t[49.29],\n\t\t[29.56]])")
+    print("Expected : array([[0.99930437],\n\t\t[1. ],\n\t\t[1. ]])")
 
     print("============= 2 ===================")
     print("loss : \n", mylr.loss_(Y,y_hat))
-    print("Expected : 553.5416083333332")
+    print("Expected : 11.513157421577002")
 
     print("============= 3 ===================")
     mylr.fit_(X, Y)
     print("thetas trained : \n", mylr.thetas)
-    print("Expected : array([[ 2.320421]\n\t\t[ -0.09432424]\n\t\t[ 5.26624478]\n\t\t[-4.12926342]\n\t\t[ 0.53598769]])")
+    print("Expected : array([[ 2.11826435]\n\t\t[ 0.10154334]\n\t\t[ 6.43942899]\n\t\t[-5.10817488]\n\t\t[ 0.6212541 ]])")
 
     print("============= 4 ===================")
     y_hat = mylr.predict_(X)
     print("predict : \n", y_hat)
-    print("Expected : array([[0.84177778]\n\t\t[1.55407511]\n\t\t[-1.29087092]])")
+    print("Expected : array([[0.57606717]\n\t\t[0.68599807]\n\t\t[0.06562156]])")
 
     print("============= 5 ===================")
     print("loss : \n", mylr.loss_(Y,y_hat))
-    print("Expected : 5.03807307639228")
+    print("Expected : 1.4779126923052268")
 
     return 0
 
